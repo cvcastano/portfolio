0208bd5972295441e869bd31f143c5a9282c1e55
cvcastano <you@example.com>
2021-04-25 13:13:08 +0200

    Update docs

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -1,1 +1,1 @@
-"use strict";console.log(">> Ready :)");
+"use strict";console.log(">> Ready :)");const navButton=document.querySelector(".js-hamburger"),navList=document.querySelector(".js-nav");function handleClick(){navList.classList.toggle("show_links")}navButton.addEventListener("click",handleClick);
