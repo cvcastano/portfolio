54e4e08bf3c19e46ae1a9e1de445e9dcbd8d2365
cvcastano <you@example.com>
2021-04-25 19:24:33 +0200

    Add arrow to scroll up

--- src/js/main.js
+++ src/js/main.js
@@ -3,12 +3,24 @@
 const header = document.querySelector('.header');
 const navButton = document.querySelector('.js-hamburger');
 const navList = document.querySelector('.js-nav');
+const main = document.querySelector('.main');
+const arrowUp = document.querySelector('.arrow-up');
+
 
 function handleClick() {
     navList.classList.toggle('show_links');
     navButton.classList.toggle('rotate');
     header.classList.toggle('dark-shadow');
+    main.classList.toggle('move-down');
 }
-
 navButton.addEventListener('click', handleClick);
 
+function scrollUp() {
+    const scrollHeight = window.pageYOffset;
+    if (scrollHeight > 300) {
+        arrowUp.classList.add('show-up');
+    } else {
+        arrowUp.classList.remove('show-up');
+    }
+}
+window.addEventListener('scroll', scrollUp);
