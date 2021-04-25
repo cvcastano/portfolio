f9b658a3c3fa986339af179975976386d54c4ee5
cvcastano <you@example.com>
2021-04-25 14:36:45 +0200

    Add shadow to openned nav

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,13 @@
 'use strict';
 
+const header = document.querySelector('.header');
 const navButton = document.querySelector('.js-hamburger');
 const navList = document.querySelector('.js-nav');
 
 function handleClick() {
     navList.classList.toggle('show_links');
     navButton.classList.toggle('rotate');
+    header.classList.toggle('dark-shadow');
 }
 
 navButton.addEventListener('click', handleClick);
