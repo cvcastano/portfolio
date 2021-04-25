f26cbfac494de5b098f34bca38195cb276f47f69
cvcastano <you@example.com>
2021-04-25 13:26:30 +0200

    Add rotation to hamburger

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,11 @@
 'use strict';
 
-console.log('>> Ready :)');
-
 const navButton = document.querySelector('.js-hamburger');
 const navList = document.querySelector('.js-nav');
 
 function handleClick() {
     navList.classList.toggle('show_links');
+    navButton.classList.toggle('rotate');
 }
 
 navButton.addEventListener('click', handleClick);
