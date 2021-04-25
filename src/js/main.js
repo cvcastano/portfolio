b383df97838a05448185d1d4b1f0db3dd00f1895
cvcastano <you@example.com>
2021-04-25 13:10:47 +0200

    Add collapsing nav bar

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
 console.log('>> Ready :)');
+
+const navButton = document.querySelector('.js-hamburger');
+const navList = document.querySelector('.js-nav');
+
+function handleClick() {
+    navList.classList.toggle('show_links');
+}
+
+navButton.addEventListener('click', handleClick);
+
